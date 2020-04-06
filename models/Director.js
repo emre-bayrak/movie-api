29f52b3e0a49c09f55319725e1c55228d59113b6
Emre <bayrakemre89@gmail.com>
2020-04-06 12:15:15 +0100

    Director Schema field validation update

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    bio: String,
+    firstName: {
+        type: String,
+        minlength: 2,
+        maxlength: 60
+    },
+    lastName: {
+        type: String,
+        minlength: 2,
+        maxlength: 60
+    },
+    bio: {
+        type:String,
+        minlength: 60,
+        maxlength: 1000
+    },
     createdAt: {
         type: Date,
         default: Date.now
