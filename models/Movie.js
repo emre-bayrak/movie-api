abe390d7ab066bae7e9400fa8cb12cbbe15d2dec
Emre <bayrakemre89@gmail.com>
2020-04-06 12:12:35 +0100

    Movie Schema field validation update

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` field is required!'],
+        minlength: [3, '`{PATH}` field (`{VALUE}`) should be more then ({MINLENGTH}) character'],
+        maxlength: [15, '`{PATH}` field (`{VALUE}`) should be less then ({MAXLENGTH}) character']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: { 
+        type: String,
+        minlength: 1,
+        maxlength: 30
+    },
+    country: {
+        type: String,
+        minlength: 1,
+        maxlength: 30
+    },
+    year: {
+        type: Number,
+        min: 1900,
+        max: 2040
+    },
+    imdb_score: {
+        type: Number,
+        min: 0,
+        max: 10
+    },
     createdAt: {
         type: Date,
         default: Date.now
